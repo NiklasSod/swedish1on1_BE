6b7bb3c5edb67acac5a70724735156248b2ac4e8
NiklasSod <nma.prob@gmail.com>
2026-02-11 17:20:26 +0100

    add zod scheema as frontend

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,14 +2,38 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
   {
-    email: { type: String, required: true, unique: true },
-    username: { type: String, required: true, unique: true },
-    passwordHash: { type: String, required: true },
-    role: { type: String, enum: ["student", "teacher"], required: true },
+    email: { 
+      type: String, 
+      required: true, 
+      unique: true,
+      trim: true,
+      lowercase: true,
+      minlength: 5,
+      maxlength: 255,
+      match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    },
+    username: { 
+      type: String, 
+      required: true, 
+      trim: true,
+      minlength: 3,
+      maxlength: 30
+    },
+    passwordHash: { 
+      type: String, 
+      required: true
+    },
+    role: { 
+      type: String, 
+      enum: ["student", "teacher"], 
+      required: true 
+    },
+    verified: {
+      type: Boolean,
+      default: false
+    },
   },
   { timestamps: true }
 );
 
 export const User = mongoose.model("User", UserSchema);
-
-// TODO do I add min length values here? Frontend has it but backend should validate too (exact same)
